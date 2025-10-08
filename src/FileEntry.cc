e326a6892244fe95ef8f9219b5ea937bf9459dd0
Michel van der Breggen <michel.van.der.breggen@tweesnoeken.nl>
2025-10-08 15:07:31 +0200

    Added some info to see how filepath are pushed along the function calls

--- src/FileEntry.cc
+++ src/FileEntry.cc
@@ -77,6 +77,7 @@ FileEntry::FileEntry(std::string path, int64_t length, int64_t offset,
       requested_(true),
       uniqueProtocol_(false)
 {
+  A2_LOG_INFO(fmt("Added new path: %s", path_.c_str()));
 }
 
 FileEntry::FileEntry()
